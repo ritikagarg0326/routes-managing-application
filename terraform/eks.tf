94b8dd05d2caeb21396bd1bca57c312c29bc7469
Ritika Garg <ritikagarg60814@gmail.com>
2026-09-19 16:53:11 +0530

    updated

---
+++ terraform/eks.tf
@@ -0,0 +1,81 @@
+# ============================================================
+# EKS CLUSTER
+#
+# Terraform creates:
+#
+# VPC
+#   ├── Private Subnet 1
+#   └── Private Subnet 2
+#            ↓
+#        EKS Cluster
+#            ↓
+#       Managed Node Group
+#            ↓
+#        EC2 Worker Node
+#
+# ============================================================
+
+resource "aws_eks_cluster" "main" {
+  name     = "${var.project_name}-eks"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  version = "1.36"
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.private_1.id,
+      aws_subnet.private_2.id
+    ]
+
+    endpoint_public_access  = true
+    endpoint_private_access = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+
+  tags = {
+    Name        = "${var.project_name}-eks"
+    Environment = var.environment
+  }
+}
+
+
+# ============================================================
+# EKS MANAGED NODE GROUP
+# ============================================================
+
+resource "aws_eks_node_group" "main" {
+  cluster_name = aws_eks_cluster.main.name
+
+  node_group_name = "${var.project_name}-ng"
+
+  node_role_arn = aws_iam_role.eks_node.arn
+
+  subnet_ids = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id
+  ]
+
+  instance_types = ["t2.small"]
+
+  capacity_type = "ON_DEMAND"
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node,
+    aws_iam_role_policy_attachment.eks_cni,
+    aws_iam_role_policy_attachment.ecr_read_only
+  ]
+
+  tags = {
+    Name        = "${var.project_name}-node"
+    Environment = var.environment
+  }
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "ecr_repository_url" {
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.logistics.repository_url
+}
+
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.main.name
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS API endpoint"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "node_group_name" {
+  description = "EKS managed node group"
+  value       = aws_eks_node_group.main.node_group_name
+}
